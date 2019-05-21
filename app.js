b7eb271192c6547df152c83d84ce403e9446e501
Techvie <49224722+Techvie@users.noreply.github.com>
2019-05-21 11:21:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const morgan = require("morgan");
 const app = express();
+const path = require('path');
+const log = console.log;
+const PORT = process.env.PORT || 4001;
 var cors = require('cors');
 const appRoute =  require('./Routes/postRoutes');
 const userRoute =  require('./Routes/userDataRouter');
 const projectRoute =  require('./Routes/projectRoutes');
 const Connections =  require('./Routes/Connections');
 
-mongoose.connect('mongodb+srv://CreativeStellars:2fEfIQ62pDb5A6QJ@cluster0-6fpb5.mongodb.net/test?retryWrites=true', { useNewUrlParser: true }, {useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://CreativeStellars:2fEfIQ62pDb5A6QJ@cluster0-6fpb5.mongodb.net/test?retryWrites=true', { useNewUrlParser: true }, {useMongoClient: true});
 //jmRbNsYEVMf5WyAI password
 app.use(bodyParser.json());
 app.use(morgan('dev'));
@@ -27,6 +30,12 @@ app.use('/api',Connections);
 
 mongoose.Promise = global.Promise;
 
-app.listen(process.env.PORT|| 8080,function(){
-    console.log("Listening to port 8080 ");
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('CLIENT/build'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(_dirname,'CLIENT','build','index.html'));
+    });   
+}
+app.listen(PORT,()=>{
+    log(`Server is starting at 4001: ${PORT}`);
 });
